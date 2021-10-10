12900661cea2d4fc5760e1e3f4a747e127084288
Justin Nguyen <45019950+nnjustin44@users.noreply.github.com>
2021-10-10 17:52:18 -0400

    init commit

---
+++ DiscordBot/Modules/Commands.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordBot.Modules;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("introduce yourself")]
+        public async Task Introduction()
+        {
+            await ReplyAsync("Hello, I am Justin's Online Native Artifical Helper; but you can call me J.O.N.A.H.");
+        }
+
+
+    }
+}
